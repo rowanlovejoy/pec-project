7fd9e2e24a9c71ce05f8bfbe134c4d4a295ff5a3
Iain <iain.carr@students.plymouth.ac.uk>
2019-03-19 11:19:36 +0000

    AdjustHeating method added with necessary variables

--- PEC-App/Assets/Iain/Scripts/TemperatureModel.cs
+++ PEC-App/Assets/Iain/Scripts/TemperatureModel.cs
@@ -5,13 +5,22 @@ using UnityEngine;
 public class TemperatureModel : MonoBehaviour
 {
     /// <summary>
-    /// The current temperature of air inside the house in degrees
+    /// Heating Period selection made by user
     /// </summary>
-    public float AirTemperature { get; }
+    public int HeatingPeriodSelection { get; set; }
+
+    /// <summary>
+    /// Thermostat Setting selection made by user
+    /// </summary>
+    public int ThermostatSettingSelection { get; set; }
+
+    private int[] m_heatingPeriod = new int[3] { 4, 6, 10 }; // How long heating is on for after coming on in ticks
+    private int[] m_thermostatSetting = new int[3] { 18, 20, 22 }; // The cutoff point for the heating in degrees
 
     // Heating Variables
     private bool m_activeHeatingPeriod = false; // true based on currentTick and heatingPeriod
     private bool m_heatingIsOn = false; // whether or not the heating is on based on activeHeatingPeriod, airTemp and thermostatSetting
+    private float m_airTemp = 18f; // the current temperature of air inside the house in degrees
 
     // Start is called before the first frame update
     void Start()
@@ -23,5 +32,37 @@ public class TemperatureModel : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void AdjustHeating(int currentTick)
+    {
+        // if currentTick is within heating period
+        if ((currentTick >= 12 && currentTick < (12 + m_heatingPeriod[HeatingPeriodSelection])) || (currentTick >= 34 && currentTick < (34 + m_heatingPeriod[HeatingPeriodSelection])))
+        {
+            m_activeHeatingPeriod = true;
+        }
+        else
+        {
+            m_activeHeatingPeriod = false;
+        }
+
+        // If activeHeatingPeriod is true and airTemp is less than thermostatSetting
+        if (m_activeHeatingPeriod && (m_airTemp < m_thermostatSetting[ThermostatSettingSelection]))
+        {
+            m_heatingIsOn = true;
+            m_airTemp += 1f;
+        }
+        else
+        {
+            m_heatingIsOn = false;
+            if (m_airTemp - 0.2f < 14)
+            {
+                m_airTemp = 14f; // air temperature should not drop below 14
+            }
+            else
+            {
+                m_airTemp -= 0.2f;
+            }
+        }
     }
 }
