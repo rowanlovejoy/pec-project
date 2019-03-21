c313fb0df660e39c310a41b384039676c62feefa
Rowan Lovejoy <44379649+rowanlovejoy@users.noreply.github.com>
2019-03-21 10:28:28 +0000

    Made AirSaturationTable.cs part of Master namespace

--- PEC-App/Assets/Master/Scripts/AirSaturationTable.cs
+++ PEC-App/Assets/Master/Scripts/AirSaturationTable.cs
@@ -2,78 +2,81 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-/// <summary>
-/// Acts as a data container for air saturation values
-/// </summary>
-public class AirSaturationTable
+namespace Master
 {
-    private readonly Dictionary<int, Dictionary<int, int>> m_wallSaturationDictionary; // holds all the values - first key returns a dictionary with all relevant data, second key returns the appropriate value
-
-    public AirSaturationTable()
-    {
-        Dictionary<int, int> firstRow = new Dictionary<int, int>()
-        {
-            [1] = 50,
-            [2] = 70,
-            [3] = 100,
-            [4] = 100,
-            [5] = 100
-        };
-
-        Dictionary<int, int> secondRow = new Dictionary<int, int>()
-        {
-            [1] = 50,
-            [2] = 70,
-            [3] = 90,
-            [4] = 100,
-            [5] = 100
-        };
-
-        Dictionary<int, int> thirdRow = new Dictionary<int, int>()
-        {
-            [1] = 50,
-            [2] = 60,
-            [3] = 80,
-            [4] = 100,
-            [5] = 100
-        };
-
-        Dictionary<int, int> fourthRow = new Dictionary<int, int>()
-        {
-            [1] = 50,
-            [2] = 50,
-            [3] = 70,
-            [4] = 90,
-            [5] = 100
-        };
-
-        Dictionary<int, int> fifthRow = new Dictionary<int, int>()
-        {
-            [1] = 50,
-            [2] = 50,
-            [3] = 70,
-            [4] = 80,
-            [5] = 100
-        };
-
-        m_wallSaturationDictionary = new Dictionary<int, Dictionary<int, int>>()
-        {
-            [14] = firstRow,
-            [16] = secondRow,
-            [18] = thirdRow,
-            [20] = fourthRow,
-            [22] = fifthRow
-        };
-    }
-
     /// <summary>
-    /// Gets the air saturation based on temperature and moisture.
+    /// Acts as a data container for air saturation values
     /// </summary>
-    /// <param name="temperature">The current air temperature in the house</param>
-    /// <param name="moistureInLitres">The current moisture in the air in whole litres</param>
-    /// <returns></returns>
-    public int GetValue(int temperature, int moistureInLitres)
+    public class AirSaturationTable
     {
-        return m_wallSaturationDictionary[temperature][moistureInLitres];
+        private readonly Dictionary<int, Dictionary<int, int>> m_wallSaturationDictionary; // holds all the values - first key returns a dictionary with all relevant data, second key returns the appropriate value
+
+        public AirSaturationTable()
+        {
+            Dictionary<int, int> firstRow = new Dictionary<int, int>()
+            {
+                [1] = 50,
+                [2] = 70,
+                [3] = 100,
+                [4] = 100,
+                [5] = 100
+            };
+
+            Dictionary<int, int> secondRow = new Dictionary<int, int>()
+            {
+                [1] = 50,
+                [2] = 70,
+                [3] = 90,
+                [4] = 100,
+                [5] = 100
+            };
+
+            Dictionary<int, int> thirdRow = new Dictionary<int, int>()
+            {
+                [1] = 50,
+                [2] = 60,
+                [3] = 80,
+                [4] = 100,
+                [5] = 100
+            };
+
+            Dictionary<int, int> fourthRow = new Dictionary<int, int>()
+            {
+                [1] = 50,
+                [2] = 50,
+                [3] = 70,
+                [4] = 90,
+                [5] = 100
+            };
+
+            Dictionary<int, int> fifthRow = new Dictionary<int, int>()
+            {
+                [1] = 50,
+                [2] = 50,
+                [3] = 70,
+                [4] = 80,
+                [5] = 100
+            };
+
+            m_wallSaturationDictionary = new Dictionary<int, Dictionary<int, int>>()
+            {
+                [14] = firstRow,
+                [16] = secondRow,
+                [18] = thirdRow,
+                [20] = fourthRow,
+                [22] = fifthRow
+            };
+        }
+
+        /// <summary>
+        /// Gets the air saturation based on temperature and moisture.
+        /// </summary>
+        /// <param name="temperature">The current air temperature in the house</param>
+        /// <param name="moistureInLitres">The current moisture in the air in whole litres</param>
+        /// <returns></returns>
+        public int GetValue(int temperature, int moistureInLitres)
+        {
+            return m_wallSaturationDictionary[temperature][moistureInLitres];
+        }
     }
 }
