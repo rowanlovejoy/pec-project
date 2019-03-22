9217cbc283a8cefd63060e2164d1aec39a55b0eb
Rowan Lovejoy <44379649+rowanlovejoy@users.noreply.github.com>
2019-03-22 10:28:22 +0000

    Made the Main Panel become visible when the simulation is stopped

--- PEC-App/Assets/Iain/Scripts/SimulationDisplayController.cs
+++ PEC-App/Assets/Iain/Scripts/SimulationDisplayController.cs
@@ -16,11 +16,20 @@ namespace Master
         private CoreAlgorithm m_coreAlgorithm = null;
 
         /// <summary>
+        /// Reference to the Main Panel
+        /// </summary>
+        [SerializeField]
+        private GameObject m_mainPanel = null;
+
+        /// <summary>
         /// Stops the simulation upon button press
         /// </summary>
         public void StopSimulation()
         {
+            /// Stops the simulation if it is ongoing
             m_coreAlgorithm.StopSimulation();
+            /// Shows the Main Panel when the simulation stops
+            m_mainPanel.SetActive(true);
         }
     }
 }
