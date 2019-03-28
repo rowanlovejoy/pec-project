b8a70b7ff339f15bb1d95f49bd9866573a1cc4e2
Iain <iain.carr@students.plymouth.ac.uk>
2019-03-28 09:46:55 +0000

    Testing in update method set up

--- PEC-App/Assets/Scripts/SpriteManager.cs
+++ PEC-App/Assets/Scripts/SpriteManager.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class SpriteManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    private GameObject[] m_moistureSpriteGroups;
+
+    [SerializeField]
+    private GameObject[] m_ventilationSpriteGroups;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Alpha1)) // FOR TESTING PURPOSES, DELETE LATER
+        {
+            UpdateMoistureProductionSprites(0);
+            UpdateVentilationSprites(0);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2)) // FOR TESTING PURPOSES, DELETE LATER
+        {
+            UpdateMoistureProductionSprites(1); 
+            UpdateVentilationSprites(1);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))// FOR TESTING PURPOSES, DELETE LATER
+        {
+            UpdateMoistureProductionSprites(2);
+            UpdateVentilationSprites(2);
+        }
     }
+
+
 }
