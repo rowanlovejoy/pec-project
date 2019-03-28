e89911c2f9fdef23433a6979d7340cd2c6610d1e
Iain <iain.carr@students.plymouth.ac.uk>
2019-03-28 09:47:48 +0000

    UpdateMoistureProductionSprites method added

--- PEC-App/Assets/Scripts/SpriteManager.cs
+++ PEC-App/Assets/Scripts/SpriteManager.cs
@@ -56,4 +56,29 @@ public class SpriteManager : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Enables a group of sprites related to moisture production and hides any that aren't selected
+    /// </summary>
+    /// <param name="selection">The selection made by the user. An integer between 0 and 2 (inclusive).</param>
+    public void UpdateMoistureProductionSprites(int selection)
+    {
+        if (m_moistureSpriteGroups.Length >= selection || selection < 0) // error handling
+        {
+            Debug.LogError("Selection made is out of bounds of array.");
+        }
+        else
+        {
+            for (int i = 0; i < m_moistureSpriteGroups.Length; i++)
+            {
+                if (i == selection) // if group has been selected
+                {
+                    m_moistureSpriteGroups[selection].SetActive(true); // enable (show) group
+                }
+                else // if group has not been selected
+                {
+                    m_moistureSpriteGroups[selection].SetActive(false); // disable (hide) group
+                }
+            }
+        }
+    }
 }
