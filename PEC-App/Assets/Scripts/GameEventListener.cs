890374be18065a48b6c35a1fa26064ef99f8941c
10203040506070809 <33032000+10203040506070809@users.noreply.github.com>
2019-03-28 14:49:54 +0000

    Added OnInspectorGUI method to GameEventListener

--- PEC-App/Assets/Scripts/GameEventListener.cs
+++ PEC-App/Assets/Scripts/GameEventListener.cs
@@ -9,7 +9,18 @@ namespace Master
     [CustomEditor(typeof(GameEvent))]
     public class GameEventListener : Editor
     {
-        
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            GUI.enabled = Application.isPlaying;
+
+            GameEvent _event = target as GameEvent;
+
+            if (GUILayout.Button("Raise"))
+            {
+                _event.Raise();
+            }
+        }
 
 
     }
