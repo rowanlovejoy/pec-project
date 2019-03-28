2d1360369437d97eebe1be6c6904c9700362c75e
Iain <iain.carr@students.plymouth.ac.uk>
2019-03-28 09:26:19 +0000

    Renamed class to SpriteManager

--- PEC-App/Assets/Scripts/SpriteManager.cs
+++ PEC-App/Assets/Scripts/SpriteManager.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpriteView : MonoBehaviour
+public class SpriteManager : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
