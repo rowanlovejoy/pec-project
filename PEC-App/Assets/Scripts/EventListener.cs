d3469e5d57763c8804bd552e0e9b4bbdf1caad33
10203040506070809 <33032000+10203040506070809@users.noreply.github.com>
2019-03-28 14:31:23 +0000

    Added m_response variable

--- PEC-App/Assets/Scripts/EventListener.cs
+++ PEC-App/Assets/Scripts/EventListener.cs
@@ -1,18 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
-public class EventListener : MonoBehaviour
+
+namespace Master
 {
-    // Start is called before the first frame update
-    void Start()
+    public class EventListener : MonoBehaviour
     {
-        
-    }
+        [SerializeField]
+        private UnityEvent m_response;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+
     }
 }
