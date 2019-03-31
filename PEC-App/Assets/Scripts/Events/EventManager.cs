978ccecb6bb76b7ece81f4ac4920f816aa5d1cde
Iain <iain.carr@students.plymouth.ac.uk>
2019-03-31 17:31:15 +0100

    StartSimulation event added

--- PEC-App/Assets/Scripts/Events/EventManager.cs
+++ PEC-App/Assets/Scripts/Events/EventManager.cs
@@ -7,6 +7,12 @@ public class EventManager : MonoBehaviour
     public static EventManager instance = null;
 
     /// <summary>
+    /// Reference to the startSimulation event
+    /// </summary>
+    [SerializeField]
+    private GameEvent m_startSimulationEvent = null;
+
+    /// <summary>
     /// Method called before Start function
     /// </summary>
     void Awake()
@@ -23,5 +29,20 @@ public class EventManager : MonoBehaviour
         }
 
         DontDestroyOnLoad(gameObject);
+    }
+
+    /// <summary>
+    /// Raises the StartSimulation event.
+    /// </summary>
+    public void RaiseStartSimulationEvent()
+    {
+        if (m_startSimulationEvent != null)
+        {
+            m_startSimulationEvent.Raise();
+        }
+        else
+        {
+            Debug.LogError("The start simulation event has not been assigned.");
+        }
     }
 }
