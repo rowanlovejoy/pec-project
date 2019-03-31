464b9e09a9161af97cf661751b3ffd2219e5167a
Iain <iain.carr@students.plymouth.ac.uk>
2019-03-31 14:37:38 +0100

    AdjsutVariables method created

--- PEC-App/Assets/Scripts/IAdjustable.cs
+++ PEC-App/Assets/Scripts/IAdjustable.cs
@@ -3,5 +3,9 @@ using System.Collections.Generic;
 
 interface IAdjustable
 {
+    /// <summary>
+    /// Checks the current tick and updates the model based on the selection settings accordingly.
+    /// </summary>
+    void AdjustVariables(int currentTick);
 
 }
