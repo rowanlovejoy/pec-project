6fc26c50b1fecaff2433f92eaad0b451080bed5f
Iain <iain.carr@students.plymouth.ac.uk>
2019-04-02 21:00:38 +0100

    EffectManager script created

---
+++ PEC-App/Assets/Scripts/Managers/EffectManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject m_kettleSteam = null;
+
+    [SerializeField]
+    private GameObject m_potSteam = null;
+
+    [SerializeField]
+    private GameObject m_bathSteam = null;
+
+    [SerializeField]
+    private GameObject[] m_condensationEffects = null;
+
+    [SerializeField]
+    private GameObject[] m_heatEffects = null;
+
+    [SerializeField]
+    private GameObject[] m_mouldEffects = null;
+
+    [SerializeField]
+    private CoreAlgorithm m_coreAlgorithm = null;
+
+   
+}
