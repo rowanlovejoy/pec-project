9ca04cb9cc9bada34e1326c091b3b6f52f75cb86
Rowan Lovejoy <44379649+rowanlovejoy@users.noreply.github.com>
2019-04-06 18:01:33 +0100

    Added array for Air Saturation rating icons to EndRatingsDisplayManager.

--- PEC-App/Assets/Scripts/EndRatingsDisplayManager.cs
+++ PEC-App/Assets/Scripts/EndRatingsDisplayManager.cs
@@ -4,5 +4,9 @@ using UnityEngine;
 
 public class EndRatingsDisplayManager : MonoBehaviour
 {
-    
+    /// <summary>
+    /// Ratings icons for the air saturation end rating.
+    /// </summary>
+    [SerializeField]
+    private GameObject[] m_airSaturationRatingIcons;
 }
