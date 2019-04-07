a437d1f73599b6206b9f0c71beba9b72bd7d1136
Rowan Lovejoy <44379649+rowanlovejoy@users.noreply.github.com>
2019-04-07 15:34:02 +0100

    Adjusted Mould particle effect.

---
+++ PEC-App/Assets/Scripts/ParticleStopTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleStopTest : MonoBehaviour
+{
+    [SerializeField]
+    private ParticleSystem m_particleSystem = null;
+
+    public void ToggleEmission()
+    {
+        if (m_particleSystem.isEmitting)
+        {
+            m_particleSystem.Stop(true, ParticleSystemStopBehavior.StopEmitting);
+        }
+        else if (!m_particleSystem.isEmitting)
+        {
+            m_particleSystem.Play();
+        }
+    }
+
+    private void Update()
+    {
+        Debug.Log("Particle emitting: " + m_particleSystem.isEmitting);
+    }
+}
