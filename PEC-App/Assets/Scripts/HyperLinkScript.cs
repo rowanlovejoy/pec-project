0c729712e80f4bef584e774d0c05b85696908535
10203040506070809 <33032000+10203040506070809@users.noreply.github.com>
2019-04-10 05:02:16 +0100

    Creation of HyperLinkScript

---
+++ PEC-App/Assets/Scripts/HyperLinkScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HyperLinkScript : MonoBehaviour
+{
+public void PECSite()
+    {
+        Application.OpenURL("https://www.plymouthenergycommunity.com/help/tips");
+
+    }
+}
