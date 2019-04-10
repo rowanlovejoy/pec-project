e12bba7ffb13c901538f6130fd0da4e1c9999574
10203040506070809 <33032000+10203040506070809@users.noreply.github.com>
2019-04-10 12:16:29 +0100

    Creation of VideoClip m_videoToPlay

--- PEC-App/Assets/Scripts/StreamVideo.cs
+++ PEC-App/Assets/Scripts/StreamVideo.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
+using UnityEngine.Video;
 public class StreamVideo : MonoBehaviour
 {
     [SerializeField]
     private RawImage m_rawImage;
+
+    [SerializeField]
+    private VideoClip m_videoToPlay;
 
 }
