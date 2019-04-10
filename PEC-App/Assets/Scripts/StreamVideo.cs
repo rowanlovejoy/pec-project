98a7d134edb8c9b08874f75a7b87930c777c59b7
10203040506070809 <33032000+10203040506070809@users.noreply.github.com>
2019-04-10 12:35:26 +0100

    Creation of playVideo method

--- PEC-App/Assets/Scripts/StreamVideo.cs
+++ PEC-App/Assets/Scripts/StreamVideo.cs
@@ -11,10 +11,10 @@ public class StreamVideo : MonoBehaviour
     [SerializeField]
     private VideoClip m_videoToPlay;
 
-    [SerializeField]
+    private VideoPlayer m_videoPlayer;
+
     private VideoSource m_videoSource;
 
-    [SerializeField]
     private AudioSource m_audioSource;
 
 
@@ -27,10 +27,50 @@ public class StreamVideo : MonoBehaviour
 
     IEnumerator playVideo()
     {
-
         
+        m_videoPlayer = gameObject.AddComponent<VideoPlayer>();
 
 
+        m_audioSource = gameObject.AddComponent<AudioSource>();
 
+        ///Disable play on awake for video/audio
+        m_videoPlayer.playOnAwake = false;
+        m_audioSource.playOnAwake = false;
+        m_audioSource.Pause();
+
+        ///Playing from file not from url
+        m_videoPlayer.source = VideoSource.VideoClip;
+
+        ///set audio output mode
+        m_videoPlayer.audioOutputMode = VideoAudioOutputMode.AudioSource;
+
+        ///Assign the Audio from Video to AudioSource
+        m_videoPlayer.EnableAudioTrack(0, true);
+        m_videoPlayer.SetTargetAudioSource(0, m_audioSource);
+
+        ///Set video to play then prepare audiio to prevent buffering
+        m_videoPlayer.clip = m_videoToPlay;
+        m_videoPlayer.Prepare();
+
+
+        ///Wait until video is prepared
+        WaitForSeconds waitTime = new WaitForSeconds(1);
+        while (!m_videoPlayer.isPrepared)
+        {
+            //Prepare/wait for 5 seconds
+            yield return waitTime;
+
+            //break after 5 seconds
+            break;
+        }
+
+        ///Assign the texture from video to rawimage to be displayed
+        m_rawImage.texture = m_videoPlayer.texture;
+
+        ///play video
+        m_videoPlayer.Play();
+
+        ///play sound
+        m_audioSource.Play();
     }
 }
