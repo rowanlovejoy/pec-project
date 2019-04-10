fcfc2459412b103fecb9e8fb438d9bf6f69abd41
10203040506070809 <33032000+10203040506070809@users.noreply.github.com>
2019-04-10 12:14:09 +0100

    Creation of StreamVideo class

---
+++ PEC-App/Assets/Scripts/StreamVideo.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StreamVideo : MonoBehaviour
+{
+    [SerializeField]
+    private RawImage m_rawImage;
+}
