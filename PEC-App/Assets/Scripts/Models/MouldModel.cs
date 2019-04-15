a628748a28f25bf0d172dfe08739257533a24c07
Iain Carr <33032036+IainSCarr@users.noreply.github.com>
2019-04-15 18:31:08 +0100

    AdjustVariables and Reset methods

--- PEC-App/Assets/Scripts/Models/MouldModel.cs
+++ PEC-App/Assets/Scripts/Models/MouldModel.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class MouldModel : IAdjustable
 {
     private MoistureModel m_moistureModel = null;
+    private int m_counter = 0;
 
     public MouldModel(MoistureModel _model)
     {
@@ -13,12 +14,28 @@ public class MouldModel : IAdjustable
 
     public void AdjustVariables(int _tick)
     {
+        if (m_moistureModel.WallSaturation >= 70)
+        {
+            m_counter++;
+        }
+        else
+        {
+            m_counter = 0;
+        }
 
+        if(m_counter >= 6)
+        {
+            EventManager.Instance.RaiseHighMouldOnEvent();
+        }
+        else
+        {
+            EventManager.Instance.RaiseHighMouldOffEvent();
+        }
     }
 
     public void ResetVariables()
     {
-
+         m_counter = 0;
     }
 
 }
