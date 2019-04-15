cfed15527b707fbd8245fa5c7e41fe41db79dff6
Iain Carr <33032036+IainSCarr@users.noreply.github.com>
2019-04-15 18:29:40 +0100

    MouldModel created

---
+++ PEC-App/Assets/Scripts/Models/MouldModel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouldModel : IAdjustable
+{
+    private MoistureModel m_moistureModel = null;
+
+    public MouldModel(MoistureModel _model)
+    {
+        m_moistureModel = _model;
+    }
+
+    public void AdjustVariables(int _tick)
+    {
+
+    }
+
+    public void ResetVariables()
+    {
+
+    }
+
+}
