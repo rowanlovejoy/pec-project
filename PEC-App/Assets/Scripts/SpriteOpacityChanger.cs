2837093a79fca7f0a71b4552b5448d9a864c6ad4
Iain Carr <33032036+IainSCarr@users.noreply.github.com>
2019-04-16 11:22:09 +0100

    Variable set up

--- PEC-App/Assets/Scripts/SpriteOpacityChanger.cs
+++ PEC-App/Assets/Scripts/SpriteOpacityChanger.cs
@@ -2,7 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(SpriteRenderer))]
 public class SpriteOpacityChanger : MonoBehaviour
 {
+    private SpriteRenderer m_sprite = null;
 
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_sprite = GetComponent<SpriteRenderer>();
+    }
 }
