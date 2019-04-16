c7c6d7a4f192b4d4291a81519b529c1af8fe5496
Iain Carr <33032036+IainSCarr@users.noreply.github.com>
2019-04-16 11:40:34 +0100

    ChangeOpacity method implemented

--- PEC-App/Assets/Scripts/SpriteOpacityChanger.cs
+++ PEC-App/Assets/Scripts/SpriteOpacityChanger.cs
@@ -7,9 +7,27 @@ public class SpriteOpacityChanger : MonoBehaviour
 {
     private SpriteRenderer m_sprite = null;
 
+    private Color m_tempColour;
+
     // Start is called before the first frame update
     void Start()
     {
         m_sprite = GetComponent<SpriteRenderer>();
+        m_tempColour = m_sprite.color;
+    }
+
+    public void ChangeOpacity(float _newValue)
+    {
+        StartCoroutine(LerpAlpha(_newValue));
+    }
+
+    IEnumerator LerpAlpha(float _targetValue)
+    {
+        while (m_tempColour.a != _targetValue)
+        {
+            m_tempColour.a = Mathf.Lerp(m_sprite.color.a, _targetValue, 1f);
+            m_sprite.color = m_tempColour;
+            yield return null;
+        }
     }
 }
