be43e6e7b201d264f983e0b2c046ea0a797789e6
Iain Carr <33032036+IainSCarr@users.noreply.github.com>
2019-04-17 11:18:57 +0100

    SwapSprites method implemented

--- PEC-App/Assets/Scripts/SpriteSwapper.cs
+++ PEC-App/Assets/Scripts/SpriteSwapper.cs
@@ -13,11 +13,27 @@ public class SpriteSwapper : MonoBehaviour
     [SerializeField]
     private Sprite m_newSprite;
 
+    private bool m_isOriginalSprite = true;
+
     // Start is called before the first frame update
     private void Start()
     {
         m_image = GetComponent<Image>();
 
         m_originalSprite = m_image.sprite;
+    }
+
+    public void SwapSprites()
+    {
+        if (m_isOriginalSprite)
+        {
+            m_image.sprite = m_newSprite;
+            m_isOriginalSprite = false;
+        }
+        else
+        {
+            m_image.sprite = m_originalSprite;
+            m_isOriginalSprite = true;
+        }
     }
 }
