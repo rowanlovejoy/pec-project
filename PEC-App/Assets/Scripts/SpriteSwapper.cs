2f7a3f82ad2437d3c456d6d0ee745a24f4f31174
Iain Carr <33032036+IainSCarr@users.noreply.github.com>
2019-04-17 11:16:48 +0100

    Variables set up

--- PEC-App/Assets/Scripts/SpriteSwapper.cs
+++ PEC-App/Assets/Scripts/SpriteSwapper.cs
@@ -3,6 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+[RequireComponent(typeof(Image))]
 public class SpriteSwapper : MonoBehaviour
 {
+    private Image m_image = null;
+
+    private Sprite m_originalSprite = null;
+
+    [SerializeField]
+    private Sprite m_newSprite;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        m_image = GetComponent<Image>();
+
+        m_originalSprite = m_image.sprite;
+    }
 }
