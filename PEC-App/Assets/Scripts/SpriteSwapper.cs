1d2a3cb64a93298fb2b90bf01bf62e355ca39c5c
Iain Carr <33032036+IainSCarr@users.noreply.github.com>
2019-04-17 11:25:57 +0100

    Comments added and variables renamed

--- PEC-App/Assets/Scripts/SpriteSwapper.cs
+++ PEC-App/Assets/Scripts/SpriteSwapper.cs
@@ -6,34 +6,49 @@ using UnityEngine.UI;
 [RequireComponent(typeof(Image))]
 public class SpriteSwapper : MonoBehaviour
 {
+    /// <summary>
+    /// Reference to Image component attached to this gameobject.
+    /// </summary>
     private Image m_image = null;
 
-    private Sprite m_originalSprite = null;
+    /// <summary>
+    /// Reference to default sprite set in Image component.
+    /// </summary>
+    private Sprite m_defaultSprite = null;
 
+    /// <summary>
+    /// Reference to new image sprite.
+    /// </summary>
     [SerializeField]
     private Sprite m_newSprite;
 
-    private bool m_isOriginalSprite = true;
+    /// <summary>
+    /// Boolean that determines state of sprite.
+    /// </summary>
+    private bool m_isDefaultSprite = true;
 
-    // Start is called before the first frame update
+    /// Start is called before the first frame update
     private void Start()
     {
         m_image = GetComponent<Image>();
 
-        m_originalSprite = m_image.sprite;
+        m_defaultSprite = m_image.sprite;
     }
 
+    /// <summary>
+    /// Swaps sprites between a default sprites and a new sprite.
+    /// </summary>
     public void SwapSprites()
     {
-        if (m_isOriginalSprite)
+        if (m_isDefaultSprite)
         {
             m_image.sprite = m_newSprite;
-            m_isOriginalSprite = false;
+            m_isDefaultSprite = false;
         }
         else
         {
-            m_image.sprite = m_originalSprite;
-            m_isOriginalSprite = true;
+            m_image.sprite = m_defaultSprite;
+            m_isDefaultSprite = true;
         }
     }
 }
