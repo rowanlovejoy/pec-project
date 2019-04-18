f13ef5d6ecf85fef4bf8c41641d946cbfd8a7305
Rowan Lovejoy <44379649+rowanlovejoy@users.noreply.github.com>
2019-04-18 11:49:26 +0100

    Added m_paramText array to ParamHelpTextManager.

--- PEC-App/Assets/Scripts/Managers/ParamHelpTextManager.cs
+++ PEC-App/Assets/Scripts/Managers/ParamHelpTextManager.cs
@@ -4,5 +4,9 @@ using UnityEngine;
 
 public class ParamHelpTextManager : MonoBehaviour
 {
-    
+    /// <summary>
+    /// Array to store help text for each parameter.
+    /// </summary>
+    [SerializeField, TextArea(3, 10)]
+    private string[] m_paramText = null;
 }
