8dea41b6bdd5bdc1e6e207548ada79ed239c737c
Rowan Lovejoy <44379649+rowanlovejoy@users.noreply.github.com>
2019-04-18 11:57:38 +0100

    Added RequireComponent attribute for TextMeshProUGUI to ParamHelpTextManager.

--- PEC-App/Assets/Scripts/Managers/ParamHelpTextManager.cs
+++ PEC-App/Assets/Scripts/Managers/ParamHelpTextManager.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
+[RequireComponent(typeof(TextMeshProUGUI))]
 public class ParamHelpTextManager : MonoBehaviour
 {
     /// <summary>
