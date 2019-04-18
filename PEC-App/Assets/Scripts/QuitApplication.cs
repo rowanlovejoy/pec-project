6d28d3cd1166727951d78e42cacf0b72865c2e51
10203040506070809 <33032000+10203040506070809@users.noreply.github.com>
2019-04-18 17:22:47 +0100

    Added Summary to QuitApplication script

--- PEC-App/Assets/Scripts/QuitApplication.cs
+++ PEC-App/Assets/Scripts/QuitApplication.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class QuitApplication : MonoBehaviour
 {
-
+    /// <summary>
+    /// Quits the application when not in editor mode when called.
+    /// </summary>
     public void QuitApplicationBtn()
     {
         Application.Quit();
