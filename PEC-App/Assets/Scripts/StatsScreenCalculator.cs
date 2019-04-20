d7cc55b81600ab53323003b3a532d6271cfde73f
10203040506070809 <33032000+10203040506070809@users.noreply.github.com>
2019-04-20 21:21:58 +0100

    Creation of m_airSat TMPro text

--- PEC-App/Assets/Scripts/StatsScreenCalculator.cs
+++ PEC-App/Assets/Scripts/StatsScreenCalculator.cs
@@ -1,14 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class StatsScreenCalculator : MonoBehaviour
 {
+    [SerializeField]
+    private TextMeshProUGUI m_airSat;
 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
+
